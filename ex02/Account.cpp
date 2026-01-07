dd11c323de887a1f929a400b409a21040712a739
Davi Simoes Dos Santos <dasimoes@c3r4p2.42sp.org.br>
2026-01-06 21:04:46 -0300

    feat: initiated ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 20:00:54 by dasimoes          #+#    #+#             */
+/*   Updated: 2026/01/06 20:14:57 by dasimoes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int	getNbAccounts( void )
+{
+
+}
+
+int	getTotalAmount( void )
+{
+
+}
+
+int	getNbDeposits( void );
+{
+
+}
+
+int	getNbWithdrawals( void );
+{
+
+}
+
+void	displayAccountsInfos( void );
+{
+
+}
+
+void	makeDeposit( int deposit );
+{
+
+}
+
+bool	makeWithdrawal( int withdrawal );
+{
+
+}
+
+int		checkAmount( void ) const;
+{
+
+}
+
+void	displayStatus( void ) const;
+{
+
+}
+
+
